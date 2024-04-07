f1a0623491d50e8521ee486f25b27da6b62272c2
Donatus Herre <donatus.herre@slub-dresden.de>
2024-04-07 18:13:58 +0200

    Add attribute found to AppDetails

--- slubfind/parser.py
+++ slubfind/parser.py
@@ -4,4 +4,5 @@ from txpyfind.parser import JSONResponse
 class AppDetails(JSONResponse):
 
     def __init__(self, plain):
+        self.found = '"id" : ""' not in plain
         super().__init__(plain)
