90831762bcac82458a3172df529aa3e33ed4992f
Donatus Herre <donatus.herre@slub-dresden.de>
2024-04-07 19:07:38 +0200

    Add attribute ok to AppDetails

--- slubfind/parser.py
+++ slubfind/parser.py
@@ -5,7 +5,5 @@ class AppDetails(JSONResponse):
 
     def __init__(self, plain):
         super().__init__(plain)
-        self.found = (isinstance(self.raw, dict) and
-                      "id" in self.raw and
-                      isinstance(self.raw["id"], str) and
-                      len(self.raw["id"].strip()) > 0)
+        self.ok = isinstance(self.raw, dict) and "id" in self.raw
+        self.found = self.ok and isinstance(self.raw["id"], str) and len(self.raw["id"].strip()) > 0
