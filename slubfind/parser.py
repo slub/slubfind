242f13995db9577a1a4b988c226cc5cdefc1e856
Donatus Herre <donatus.herre@slub-dresden.de>
2024-11-24 22:19:56 +0100

    just some formatting

--- slubfind/parser.py
+++ slubfind/parser.py
@@ -9,11 +9,8 @@ class AppDetails(JSONResponse):
     ``AppDetails`` class from ``slubfind.parser`` module
     """
 
-    def __init__(
-            self,
-            plain):
+    def __init__(self, plain):
         super().__init__(plain)
-        self.ok = isinstance(
-            self.raw, dict) and "id" in self.raw
+        self.ok = isinstance(self.raw, dict) and "id" in self.raw
         self.found = self.ok and isinstance(
             self.raw["id"], str) and len(self.raw["id"].strip()) > 0
