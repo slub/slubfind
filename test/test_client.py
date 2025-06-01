f7af8eb65fb978c13a434747da06c00b85409c77
Donatus Herre <donatus.herre@slub-dresden.de>
2025-06-01 22:21:10 +0200

    Deactivate tests for solr_params
    See #12 for details.

--- test/test_client.py
+++ test/test_client.py
@@ -21,19 +21,19 @@ def test_settings():
     slub_find_settings = slub_find.settings()
     assert slub_find_settings is not None
 
-def test_solr_params():
-    # create SlubFind instance
-    slub_find = SlubFind()
-    # retrieve Solr parameters (query view)
-    slub_find_solr_params = slub_find.solr_params("manfred bonitz")
-    assert slub_find_solr_params is not None
+#def test_solr_params():
+#    # create SlubFind instance
+#    slub_find = SlubFind()
+#    # retrieve Solr parameters (query view)
+#    slub_find_solr_params = slub_find.solr_params("manfred bonitz")
+#    assert slub_find_solr_params is not None
 
-def test_solr_params_via_url():
-    # create SlubFind instance
-    slub_find = SlubFind()
-    # retrieve Solr parameters (query view)
-    slub_find_solr_params_via_url = slub_find.solr_params_via_url("https://katalog.slub-dresden.de/?tx_find_find%5Bq%5D%5Bdefault%5D=manfred+bonitz")
-    assert slub_find_solr_params_via_url is not None
+#def test_solr_params_via_url():
+#    # create SlubFind instance
+#    slub_find = SlubFind()
+#    # retrieve Solr parameters (query view)
+#    slub_find_solr_params_via_url = slub_find.solr_params_via_url("https://katalog.slub-dresden.de/?tx_find_find%5Bq%5D%5Bdefault%5D=manfred+bonitz")
+#    assert slub_find_solr_params_via_url is not None
 
 def test_solr_request():
     # create SlubFind instance
